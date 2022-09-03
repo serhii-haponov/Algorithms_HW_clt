eb2f43157c87c87e0b39f18d1d625eb24a29295f
Serhii Haponov <serhii.haponov@chisw.com>
2022-09-03 08:32:42 +0300

    HW3_CF_BBooks done

--- Algorithms_HW_clt/main.swift
+++ Algorithms_HW_clt/main.swift
@@ -12,5 +12,6 @@ print("Hello, World!")
 //HW3_T1().start()
 //[4,3,2,1,4])) //17
 //print(HW3_T2().amountOfSubArraysFor2(sum: 7, in: [2, 4, 1, 2, 7]))
-HW3_HR_1_10_100().start()
+//HW3_HR_1_10_100().start()
 //HW3_HR_Raskin_Bobbins().start()
+HW3_CF_BBooks().start()
