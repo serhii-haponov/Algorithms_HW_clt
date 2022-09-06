9347c3ec98d58b33a5faa6a8dc201eacfe33bc95
Serhii Haponov <serhii.haponov@chisw.com>
2022-09-06 20:27:44 +0300

    start autoloan

---
+++ Algorithms_HW_clt/HW3/HW3_HR_Auto_Loan.swift
@@ -0,0 +1,32 @@
+//
+//  HW3_HR_Auto_Loan.swift
+//  Algorithms_HW_clt
+//
+//  Created by Serhii Haponov on 06.09.2022.
+//
+
+import Foundation
+
+//MARK: - Start
+struct HW3_HR_Auto_Loan {
+    
+    func start() {
+        let input = getInput()
+        print()
+    }
+}
+
+//MARK: - Input
+private extension HW3_HR_Auto_Loan {
+    func getInput() -> Int {
+        print("Set imput for HW3_HR_Auto_Loan:")
+        let firstLine = readLine()?.split(separator: " ")
+        return 1
+    }
+}
+
+//MARK: - Execution
+private extension HW3_HR_Auto_Loan {
+    
+    
+}
