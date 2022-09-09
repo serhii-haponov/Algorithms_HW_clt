2253e406ab53e2e2a655a400e1f867b0b0022658
Serhii Haponov <serhii.haponov@chisw.com>
2022-09-09 15:10:48 +0300

    rename most water

--- Algorithms_HW_clt/HW3/HW3_Most_Water.swift
+++ Algorithms_HW_clt/HW3/HW3_Most_Water.swift
@@ -1,5 +1,5 @@
 //
-//  HW3_T1.swift
+//  HW3_Most_Water.swift
 //  Algorithms_HW_clt
 //
 //  Created by Serhii Haponov on 22.08.2022.
@@ -7,7 +7,39 @@
 
 import Foundation
 
-struct HW3_T1 {
+//https://leetcode.com/problems/container-with-most-water/
+
+//You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+//
+//Find two lines that together with the x-axis form a container, such that the container contains the most water.
+//
+//Return the maximum amount of water a container can store.
+//
+//Notice that you may not slant the container.
+//
+//
+//
+//Example 1:
+//
+//
+//Input: height = [1,8,6,2,5,4,8,3,7]
+//Output: 49
+//Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+//Example 2:
+//
+//Input: height = [1,1]
+//Output: 1
+//
+//
+//Constraints:
+//
+//n == height.length
+//2 <= n <= 105
+//0 <= height[i] <= 104
+
+
+
+struct HW3_Most_Water {
     
     func maxArea1(_ height: [Int]) -> Int {
         var result = 0
