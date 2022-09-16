1738618cdd58a7c23a85fba561f6cf3432af85f3
Serhii Haponov <serhii.haponov@chisw.com>
2022-09-16 08:09:50 +0300

    hw4 sortedSquares done

--- Algorithms_HW_clt/HW4/HW4_SortedSquares.swift
+++ Algorithms_HW_clt/HW4/HW4_SortedSquares.swift
@@ -8,8 +8,24 @@
 import Foundation
 
 struct HW4_SortedSquares {
-    
-    func sortedSquares(_ nums: [Int]) -> [Int] {
-         return []
-     }
+    //    [-4,-1,0,3,10]
+    func sortedSquares(_ nums: [Int]) -> [Int] { //O(N)
+        var array: [Int] = nums
+        var j = nums.count - 1
+        
+        for i in 0..<nums.count {
+            guard j - i >= 0 else { return array }
+            let ii = nums[i] * nums[i]
+            var jj = nums[j] * nums[j]
+            while  jj > ii {
+                array[j - i] = jj
+                j -= 1
+                jj = nums[j] * nums[j]
+            }
+            array[j - i] = ii
+        }
+        
+        return array
+    }
 }
+
