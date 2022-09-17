bec8eb06f86262b19527f35fb38bf16bbdb9fecf
Serhii Haponov <serhii.haponov@chisw.com>
2022-09-18 01:46:29 +0300

    on stage to note by pencil

--- Algorithms_HW_clt/HW4/HW4_Lvivstar.swift
+++ Algorithms_HW_clt/HW4/HW4_Lvivstar.swift
@@ -38,7 +38,7 @@ struct HW4_Lvivstar {
     
     func start() {
         let input = getInput()
-        executeCommands(input.baseStations, input.commands)
+        executeCommands2(input.baseStations, input.commands)
         
     }
 }
@@ -71,13 +71,13 @@ private extension HW4_Lvivstar {
     //brute_forse
     func executeCommands(_ baseStations: [Int], _ commands: [CommandsType]) {
         var stations = baseStations
-    
+        
         for com in commands {
             switch com {
             case .COUNT(let fromId, let toId):
                 var counter = 0
                 for i in (fromId - 1)..<toId {
-                  counter += stations[i]
+                    counter += stations[i]
                 }
                 print(counter)
             case .ENTER(let id):
@@ -88,5 +88,90 @@ private extension HW4_Lvivstar {
                 print("error")
             }
         }
+    }
+    
+    //    2 0 2 3 1
+    func executeCommands2(_ baseStations: [Int], _ commands: [CommandsType]) {
+        
+        let sqrtInterval = Int(sqrt(Double(baseStations.count)))
+        var intervals: [Int] = []
+        var sum = 0
+        
+        for i in 0..<baseStations.count {
+            if (i % sqrtInterval == 0 && i != 0) {
+                intervals.append(sum)
+                sum = baseStations[i]
+                if i == baseStations.count - 1 {
+                    intervals.append(sum)
+                }
+            } else {
+                sum += baseStations[i]
+            }
+        }
+        
+        func positionInInterval(for id: Int) -> (intervalId: Int, idPosition: Int) {
+            let intervalId = (id + 1) / sqrtInterval - 1
+            let idPosition = (id - 1) % sqrtInterval
+            return (intervalId, idPosition)
+        }
+        
+        var baseStations = baseStations
+        
+        for com in commands {
+            switch com {
+            case .COUNT(let fromId, let toId):
+                
+                let lowPosition = positionInInterval(for: fromId)
+                let upperPosition = positionInInterval(for: toId)
+                
+                if lowPosition.intervalId != upperPosition.intervalId {
+                    
+                    var lowSum = intervals[lowPosition.intervalId]
+                    for i in 0..<lowPosition.idPosition {
+                        lowSum -= (baseStations[fromId - 1 - lowPosition.idPosition + i])
+                    }
+                    
+                    var upperSum = intervals[upperPosition.intervalId]
+                    for i in upperPosition.idPosition...sqrtInterval {
+                        guard toId + i < baseStations.count else { break }
+                        upperSum -= (baseStations[toId + i])
+                    }
+                    
+                    var sum = lowSum + upperSum
+                    
+                    for i in (lowPosition.intervalId + 1)..<upperPosition.intervalId {
+                        sum += intervals[i]
+                    }
+                    
+                    print(sum)
+                } else {
+                    print("implement one grab solution")
+                }
+                
+                
+                //                let lowPositionInterval = (fromId + 1) / sqrtInterval - 1
+                //                let positionInInterval =  (fromId - 1) % sqrtInterval
+                
+                //                var lowSum = intervals[lowPosition.intervalId]
+                //                for i in 0..<lowPosition.idPosition {
+                //                    lowSum -= (baseStations[fromId - 1 - lowPosition.idPosition + i])
+                //                }
+                
+                //                print(counter)
+            case .ENTER(let id):
+                print("ENTER")
+                baseStations[id - 1] += 1
+                let position = positionInInterval(for: id)
+                intervals[position.intervalId] += 1
+            case .LEAVE(let id):
+                print("LEAVE")
+                baseStations[id - 1] -= 1
+                let position = positionInInterval(for: id)
+                intervals[position.intervalId] -= 1
+            case .unknowed:
+                print("error")
+            }
+        }
+        
     }
 }
